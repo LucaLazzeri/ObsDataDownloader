2688903a48d10d4a771ffb734f61632b647f7c4c
lazzeri <lucalazzeri97@gmail.com>
2019-10-15 14:59:29 +0200

    Instantiate Directory

---
+++ ObsDataDownloader/ObsDataDownloader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace ObsDataDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string str = client.DownloadString("https://cdn.younow.com/php/api/broadcast/audience/broadcaster=0/channelId=35771221");
+            JObject json = JObject.Parse(str);
+
+            Console.WriteLine(json.ToString());
+
+        }
+    }
+}
